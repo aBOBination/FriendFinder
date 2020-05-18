79ed9806cce8da84bd90ee285224940b7040d757
Robert Marks <rmarks@nav.com>
2020-05-17 19:27:42 -0600

    routes started

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,15 @@
-//
+// Your htmlRoutes.js file should include two routes:
+
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the
+// home page.
+
+// var app = require('../../server.js');
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../public/home.html'));
+// });
+
+// app.get('/add', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../public/survey.html'));
+// });
