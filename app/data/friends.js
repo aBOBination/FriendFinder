ab6545cc148a7b6cda9b639035600c2b5b059cb2
Robert Marks <rmarks@nav.com>
2020-05-26 00:07:54 -0600

    new data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,32 @@
 var friends = [
   {
-    name: 'Static Bobby',
+    name: 'Bobby',
     photo: 'https://i.imgur.com/0LCPOt0.jpg',
     scores: [4, 4, 4, 3, 4, 2, 4, 2, 2, 1],
+  },
+  {
+    name: 'Dr. Peter Venkman',
+    photo:
+      'https://pm1.narvii.com/6943/3b34700da20477727fe152d14de1363d7866f66dr1-1018-1024v2_uhq.jpg',
+    scores: [1, 4, 4, 1, 1, 5, 2, 5, 2, 1],
+  },
+  {
+    name: 'Dr. Raymond Stantz',
+    photo:
+      'https://i.pinimg.com/originals/6f/ac/cf/6faccf10d3202ec735587330dde490cc.jpg',
+    scores: [4, 1, 5, 2, 5, 5, 3, 5, 4, 4],
+  },
+  {
+    name: 'Dr. Egon Spengler',
+    photo:
+      'https://66.media.tumblr.com/06bee9feb149cee193d3bb4dc8b3a3fb/tumblr_inline_oxnt4xBDPd1undx97_540.jpg',
+    scores: [3, 1, 3, 4, 3, 2, 1, 4, 4, 3],
+  },
+  {
+    name: 'Winston Zeddemore',
+    photo:
+      'https://cdn1-www.comingsoon.net/assets/uploads/2019/09/Ernie-Hudson-as-Winston-in-Ghostbusters.jpg',
+    scores: [2, 1, 2, 3, 3, 5, 4, 1, 4, 2],
   },
 ];
 
